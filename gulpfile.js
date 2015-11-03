ec2db32f67adfcdf8ac23b073ad919eb095ae626
jakhead <jakhead@gmail.com>
2015-11-03 00:32:25 -0600

    gulp compress js/css

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var notify = require('gulp-notify');
 var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
 
-var DEST = 'assets/js/';
+var jsDEST = 'assets/js/';
+var cssDEST = 'assets/css/'
 
-gulp.task('default', function() {
+gulp.task('js', function() {
+    //concat and minify js
     return gulp.src(['assets/js/binomial.js',
                      'assets/js/jquery.js',
                      'assets/js/jquery-ui.min.js',
@@ -19,6 +22,20 @@ gulp.task('default', function() {
                      'assets/js/app.js'])
         .pipe(concat('dieStats.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest(DEST))
-        .pipe(notify({ message: 'Finished concat and uglifying'}));
+        .pipe(gulp.dest(jsDEST))
+        .pipe(notify({ message: 'Finished compressing javascript' }));
+});
+
+gulp.task('css', function() {
+    //concat and minify css
+    return gulp.src(['assets/css/font-awesome.min.css',
+                     'assets/css/normalize.css',
+                     'assets/css/uikit.gradient.min.css',
+                     'assets/css/jquery-ui.min.css',
+                     'assets/css/nouislider.min.css',
+                     'assets/css/custom.css'])
+        .pipe(concat('dieStats.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest(cssDEST))
+        .pipe(notify( { message: 'Finished compressing css' }))
 });
